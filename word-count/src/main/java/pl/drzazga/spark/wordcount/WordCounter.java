7c8d2d179b9e5b160fb8b5c6cdaa673ebefc60bb
drzazga <bartek4674@gmail.com>
2017-07-26 23:38:41 +0200

    Added bible file. Reduced result size.

--- word-count/src/main/java/pl/drzazga/spark/wordcount/WordCounter.java
+++ word-count/src/main/java/pl/drzazga/spark/wordcount/WordCounter.java
@@ -17,7 +17,7 @@ public class WordCounter {
     private static final String MASTER = "local[*]";
     
 	public static void main(String[] args) {
-	    new WordCounter().calculate("/words.txt");
+	    new WordCounter().calculate("/bible.txt");
 	}
 	
     private void calculate(String resourceFileName) {
@@ -26,12 +26,14 @@ public class WordCounter {
         try (JavaSparkContext context = new JavaSparkContext(conf)) {
             context.textFile(getClass().getResource(resourceFileName).toURI().toString())
                    .flatMap(text -> Arrays.asList(text.split(" ")).iterator())
-                   .mapToPair(word -> new Tuple2<>(word, 1))
+                   .filter(s -> s.length() > 0)
+                   .mapToPair(word -> new Tuple2<>(word.toLowerCase(), 1))
                    .reduceByKey(Integer::sum)
                    .mapToPair(x -> x.swap())
                    .sortByKey(false, 1)
                    .mapToPair(x -> x.swap())
-                   .foreach(result -> log.info(String.format("%s => %d", result._1(), result._2)));
+                   .take(10)
+                   .forEach(result -> log.info(String.format("'%s' => %d", result._1(), result._2)));
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
